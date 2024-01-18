c04d5a6e36d4727ca9bde0063a0a372f2be13707
lde-mich <luca01112003@gmail.com>
2024-01-18 14:05:02 +0100

    upload-gen-18

--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -6,16 +6,21 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 11:18:39 by lde-mich          #+#    #+#             */
-/*   Updated: 2024/01/18 11:19:41 by lde-mich         ###   ########.fr       */
+/*   Updated: 2024/01/18 14:04:05 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include "ClapTrap.hpp"
 
-class ScavTrap
+class ScavTrap: public ClapTrap
 {
 	private:
 
 	public:
-	
+		void guardGate();
+
+		ScavTrap(): ClapTrap(){}
+		ScavTrap(std::string name): ClapTrap(name){}
+		~ScavTrap();
 };
