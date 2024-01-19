2c73f2a7ea651ff705975e0aea54876de5455478
lde-mich <luca01112003@gmail.com>
2024-01-19 09:05:07 +0100

    upload-gen-19

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,12 +6,13 @@
 /*   By: lde-mich <lde-mich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 18:12:41 by lde-mich          #+#    #+#             */
-/*   Updated: 2024/01/18 15:56:56 by lde-mich         ###   ########.fr       */
+/*   Updated: 2024/01/19 09:02:54 by lde-mich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
 int main ()
 {
@@ -19,6 +20,8 @@ int main ()
     ClapTrap b("Aldisti");
 	
 	ScavTrap c("Dcastagn");
+
+    FragTrap d("Gpanico");
 
     a.attack("Aldisti");
     b.attack("Lde-mich");
